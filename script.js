fa391a92768ea6b7eb78f203fed0fee91136d75b
Code4Daze <jevon.fillette@gmail.com>
2022-07-06 14:12:10 -0400

    Add CSS to header and create theme changer

---
+++ script.js
@@ -0,0 +1,21 @@
+let darkThemeBtn = document.querySelector('.darkTheme');
+let lightThemeBtn = document.querySelector('.lightTheme');
+let themeSelectLink = document.querySelector('#themeSelect');
+let currentTheme = localStorage.getItem("theme") || "light";
+
+darkThemeBtn.addEventListener('click', () => setTheme("dark"));
+lightThemeBtn.addEventListener('click', () => setTheme("light"));
+
+function setTheme(themeName) {
+  themeSelectLink.setAttribute('href', `./css/${themeName}.css`);
+  window.localStorage.setItem("theme", `${themeName}`);
+  if (themeName === "dark") {
+    darkThemeBtn.classList.add('hidden');
+    lightThemeBtn.classList.remove('hidden');
+  } else {
+    lightThemeBtn.classList.add('hidden');
+    darkThemeBtn.classList.remove('hidden');
+  }
+}
+
+setTheme(currentTheme);
