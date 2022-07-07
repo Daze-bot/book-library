c12b444717cb88cce6827274f4a85d9a5607589d
Code4Daze <jevon.fillette@gmail.com>
2022-07-07 14:02:37 -0400

    Edit css for read button hover

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ let lightThemeBtn = document.querySelector('.lightTheme');
 let themeSelectLink = document.querySelector('#themeSelect');
 let currentTheme = localStorage.getItem("theme") || "light";
 let toTopBtn = document.querySelector('.toTop');
-let checkMark = document.querySelector('.checkMark');
-let readStatusBtn = document.querySelector('.readCheck');
-let bookCard = document.querySelector('.bookCard');
+let readStatusBtns = document.querySelectorAll('.readCheck');
 
 darkThemeBtn.addEventListener('click', () => setTheme("dark"));
 lightThemeBtn.addEventListener('click', () => setTheme("light"));
@@ -13,15 +11,15 @@ lightThemeBtn.addEventListener('click', () => setTheme("light"));
 window.addEventListener('scroll', showToTop);
 toTopBtn.addEventListener('click', () => {window.scrollTo(0, 0)});
 
-readStatusBtn.addEventListener('click', markRead);
+readStatusBtns.forEach(readStatusBtn => readStatusBtn.addEventListener('click', markRead))
 
 function markRead() {
-  if (checkMark.classList.contains('hidden')) {
-    checkMark.classList.remove('hidden');
-    bookCard.classList.add('readBackground');
+  if (this.children[0].classList.contains('hidden')) {
+    this.children[0].classList.remove('hidden');
+    this.parentElement.parentElement.classList.add('readBackground');
   } else {
-    checkMark.classList.add('hidden');
-    bookCard.classList.remove('readBackground');
+    this.children[0].classList.add('hidden');
+    this.parentElement.parentElement.classList.remove('readBackground');
   }
 }
 
