1401313d627f38f0d7df6ab3c5f5853eadfd53f4
Code4Daze <jevon.fillette@gmail.com>
2022-07-07 13:31:06 -0400

    Add read status button to book cards

--- script.js
+++ script.js
@@ -2,9 +2,36 @@ let darkThemeBtn = document.querySelector('.darkTheme');
 let lightThemeBtn = document.querySelector('.lightTheme');
 let themeSelectLink = document.querySelector('#themeSelect');
 let currentTheme = localStorage.getItem("theme") || "light";
+let toTopBtn = document.querySelector('.toTop');
+let checkMark = document.querySelector('.checkMark');
+let readStatusBtn = document.querySelector('.readCheck');
+let bookCard = document.querySelector('.bookCard');
 
 darkThemeBtn.addEventListener('click', () => setTheme("dark"));
 lightThemeBtn.addEventListener('click', () => setTheme("light"));
+
+window.addEventListener('scroll', showToTop);
+toTopBtn.addEventListener('click', () => {window.scrollTo(0, 0)});
+
+readStatusBtn.addEventListener('click', markRead);
+
+function markRead() {
+  if (checkMark.classList.contains('hidden')) {
+    checkMark.classList.remove('hidden');
+    bookCard.classList.add('readBackground');
+  } else {
+    checkMark.classList.add('hidden');
+    bookCard.classList.remove('readBackground');
+  }
+}
+
+function showToTop() {
+  if (window.scrollY >= 210) {
+    toTopBtn.classList.remove('hidden');
+  } else {
+    toTopBtn.classList.add('hidden');
+  }
+}
 
 function setTheme(themeName) {
   themeSelectLink.setAttribute('href', `./css/${themeName}.css`);
@@ -19,17 +46,3 @@ function setTheme(themeName) {
 }
 
 setTheme(currentTheme);
-
-let toTopBtn = document.querySelector('.toTop');
-
-function showToTop() {
-  if (window.scrollY >= 210) {
-    toTopBtn.classList.remove('hidden');
-  } else {
-    toTopBtn.classList.add('hidden');
-  }
-}
-
-window.addEventListener('scroll', showToTop);
-
-toTopBtn.addEventListener('click', () => {window.scrollTo(0, 0)});
